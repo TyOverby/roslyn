44b311baddfc272daa38c698ade10a63db363339
Ty Overby <ty@pre-alpha.com>
2016-12-05 22:59:10 +0000

    add repro file

---
+++ repro.cs
@@ -0,0 +1,4 @@
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Reflection.AssemblyVersion("1.9.0")]
+[assembly: System.Reflection.AssemblyProduct("My Repro Product")]
+namespace N {}
